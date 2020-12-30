740908760932833f9510ec00e9e6b5a03f217dab
itdxy <237990704@qq.com>
2020-12-30 17:07:34 +0800

    user功能完成

--- assets/js/index.js
+++ assets/js/index.js
@@ -37,18 +37,6 @@ function getUserInfo() {
             //调用 renderAvatar 渲染用户头像
             renderAvatar(res.data);
         },
-        // 不论成功还是失败，最终都会调用 complete 回调函数
-        complete: function (res) {
-            // console.log('执行了 complete 回调：')
-            // console.log(res)
-            // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
-            if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
-                // 1. 强制清空 token
-                localStorage.removeItem('token')
-                // 2. 强制跳转到登录页面
-                location.href = '/login.html'
-            }
-        }
 
     })
 }
@@ -61,7 +49,8 @@ function renderAvatar(user) {
     //3.按需上传用户的头像
     if (user.user_pic !== null) {
         //3.1 渲染图片头像
-        $('.layui-nav-img').attr('src', user.user.pic).show();
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        $('.text-avatar').hide()
     } else {
         //3.2渲染文本头像
         $('.layui-nav-img').hide();
